3c32dbfc19b76f8ebadc536f228b2ed6b7e4bf7e
Peter Mashin <petemashine@gmail.com>
2021-09-22 08:40:48 +0300

    pied piper update

--- pied piper/script.js
+++ pied piper/script.js
@@ -1,3 +1,14 @@
-const spacer = document.querySelector('.spacer') ;
-const header = document.querySelector('.header') ;
-// spacer.style.height = `${header.clientHeight}px`
+// const spacer = document.querySelector('.spacer') ;
+const header = document.querySelector('.header').offsetHeight ;
+const jumbotron = document.querySelector('.jumbotron').getBoundingClientRect().height ;
+// const header = document.querySelector('.header') ;
+// const header = document.querySelector('.header') ;
+console.log(header) ;
+console.log(jumbotron) ;
+
+
+
+const problemAnchor= document.querySelector('.problemAnchor') ;
+
+// problemAnchor.top = ${(problem - header)}px ;
+// console.log(problemAnchor.top)
