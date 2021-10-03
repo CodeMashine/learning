444013d0b9ff0bd7761e66de3994b6292eac5e75
Peter Mashin <petemashine@gmail.com>
2021-10-03 19:31:47 +0300

    pied piper complite

--- pied piper/script.js
+++ pied piper/script.js
@@ -1,36 +1,29 @@
-function slowScroll(event){
+function slowScroll(event) {
 
-    if(event.target.tagName==='A'){
-        event.preventDefault() ;
+    if (event.target.tagName === 'A') {
+        event.preventDefault();
 
-        const elem = event.target.getAttribute('href').slice(1) ;
+        const elem = event.target.getAttribute('href').slice(1);
         window.scrollTo({
-            top: `${yPos()}` ,
-            behavior : 'smooth' ,
+            top: `${yPos()}`,
+            behavior: 'smooth',
         })
 
-        function yPos(){
-        const prop = document.querySelector(`.${elem}`) ;
-        return prop.offsetTop - headerHeight  ;
+        function yPos() {
+            const prop = document.querySelector(`.${elem}`);
+            return prop.offsetTop - headerHeight;
         }
-           
-    }else if(event.target.closest('.logo')){
+    } else if (event.target.closest('.logo')) {
         window.scrollTo({
-            top:0 ,
-            behavior:'smooth' ,
+            top: 0,
+            behavior: 'smooth',
         })
     }
-
-
-
-
 }
-const header=document.querySelector('.header');
+const header = document.querySelector('.header');
 
-const headerHeight=document.querySelector('.header').getBoundingClientRect().height ;
-const problem=document.querySelector('.problem').getBoundingClientRect().y ;
-const ppc=document.querySelector('.PPC').getBoundingClientRect().y ;
-const hero=document.querySelector('.hero') ;
+const headerHeight = document.querySelector('.header').getBoundingClientRect().height;
 
-
-header.addEventListener('click' , (event)=>{slowScroll(event) } )
+header.addEventListener('click', (event) => {
+    slowScroll(event)
+})
